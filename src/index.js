650d81c2103619abdb660c17c14450d9dc92bf87
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 10:43:09 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 23-balancedBrackets
+Difficulty:  Intermediate
+Prompt:
+- Write a function called balancedBrackets that accepts a single string as argument.
+- The input string is composed entirely of parentheses, brackets and/or curly braces, i.e.,  (), [] and/or {}. Referred to as "braces" from this point forward...
+- The balancedBraces function should return true if the string's braces are "balanced" and false if they are not.
+- The brackets are considered unbalanced if any closing bracket does not close the same type of opening bracket, ignoring already matched brackets between them.  Examples explain it best...
+Examples:
+balancedBrackets( '()' ) // => true
+balancedBrackets( '(]' ) // => false
+balancedBrackets( '[{}]' ) // => true
+balancedBrackets( '[(])' ) // => false
+balancedBrackets( '[({}[])]' ) // => true
+-----------------------------------------------------------------*/
+// Your solution for 23-balancedBrackets here:
+
+
+</div>
+`;
